392d9bf5b998173ff75a650989ef2dd5b0f40389
Paweł Jurek <pawel80121@gmail.com>
2024-03-27 21:36:57 +0100

    winner function tests

--- AI_mini_projects/tictactoe/tests.py
+++ AI_mini_projects/tictactoe/tests.py
@@ -16,6 +16,30 @@ board3 = [[X, O, X],
           [O, O, X],
           [O, X, X]]
 
+board4 = [[X, X, O],
+          [O, O, X],
+          [O, X, X]]
+
+board5 = [[X, O, X],
+          [O, X, O],
+          [O, X, X]]
+
+board6 = [[O, X, X],
+          [O, O, O],
+          [O, X, X]]
+
+board7 = [[X, EMPTY, X],
+          [O, X, O],
+          [EMPTY, O, X]]
+
+board8 = [[X, EMPTY, EMPTY],
+          [EMPTY, X, EMPTY],
+          [EMPTY, EMPTY, X]]
+
+board9 = [[EMPTY, EMPTY, O],
+          [EMPTY, O, EMPTY],
+          [O, EMPTY, EMPTY]]
+
 class PlayerFunctionTest(unittest.TestCase):
     def test_moreX(self):
         result = player(board1)
@@ -70,6 +94,45 @@ class ResultFunctionTest(unittest.TestCase):
             exception = True
         finally:
             self.assertTrue(exception)
+
+
+class WinnerFunctionTest(unittest.TestCase):
+    def test_winningX1(self):
+        result = winner(board1)
+        self.assertEqual(result, X)
+
+    def test_no_winner(self):
+        result = winner(board2)
+        self.assertEqual(result, None)
+
+    def test_winningX2(self):
+        result = winner(board3)
+        self.assertEqual(result, X)
+
+    def test_winningO1(self):
+        result = winner(board4)
+        self.assertEqual(result, O)
+
+    def test_winningX3(self):
+        result = winner(board5)
+        self.assertEqual(result, X)
+
+    def test_winningO2(self):
+        result = winner(board6)
+        self.assertEqual(result, O)
+
+    def test_winningX4(self):
+        result = winner(board7)
+        self.assertEqual(result, X)
+    
+    def test_winningX5(self):
+        result = winner(board8)
+        self.assertEqual(result, X)
+
+    def test_winningO3(self):
+        result = winner(board9)
+        self.assertEqual(result, O)
+
 
 if __name__ == '__main__':
     unittest.main()

0d9ddd070f10544b4cbaa64f63ad45d3f3df94ec
Paweł Jurek <pawel80121@gmail.com>
2024-03-29 21:05:46 +0100

    puzzle 2

--- AI_mini_projects/knights/puzzle.py
+++ AI_mini_projects/knights/puzzle.py
@@ -39,7 +39,19 @@ knowledge1 = And(
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+
+    Or(
+        And(AKnave, Not(And(AKnave, BKnave))),  
+        And(AKnight, And(AKnave, BKnave)) 
+    ),
+    Or(
+        And(BKnave, And(AKnave, BKnave)),  
+        And(BKnight, Not(And(AKnave, BKnave))) 
+    )
 )
 
 # Puzzle 3
